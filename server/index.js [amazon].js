aee60a45bc169924e188a8e1e7343d781a8ada35
roy <roybout@gmail.com>
2017-10-01 18:52:02 +1100

    removed duplicate lines

--- server/index.js [amazon].js
+++ server/index.js [amazon].js
@@ -35,43 +35,6 @@ app.listen( app.get( 'port' ), function() {
 	console.log( 'Node app is running on port', app.get( 'port' ) );
 } );
 
-var express = require( 'express' );
-var app = express();
-var bodyParser = require( 'body-parser' );
-var pg = require( 'pg' );
-const {
-	Pool,
-	Client
-} = require( 'pg' )
-
-app.use( bodyParser.json() );
-app.use( bodyParser.urlencoded( {
-	extended: true
-} ) );
-
-//Add database to dev set up
-const pool = new Pool( {
-	user: 'redtree',
-	host: 'poc-ndis-tooling.cmo2rmmukfwp.ap-southeast-2.rds.amazonaws.com',
-	database: 'poc_db_ndis_tooling',
-	password: '3h76fRMZcDigfTvf',
-	port: 5432
-} );
-
-app.set( 'port', 80 );
-app.use( express.static( __dirname + '/dist/n-tooling-prod' ) );
-app.set( 'views', __dirname + '/dist/n-tooling-prod' );
-app.engine( 'html', require( 'ejs' ).renderFile );
-app.set( 'view engine', 'html' );
-
-app.get( '/', function( req, res ) {
-	res.render( './index.html' )
-} );
-
-app.listen( app.get( 'port' ), function() {
-	console.log( 'Node app is running on port', app.get( 'port' ) );
-} );
-
 /**
  * BEGIN APIs
  */
